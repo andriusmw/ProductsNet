43877353ca636d0a67c3b28b27241d4767c62b06
andriusmw <andriusmw@yahoo.es>
2024-02-15 16:33:15 +0100

    ProductosContext + Producto class ammends

--- ProductosNET/Models/Producto.cs
+++ ProductosNET/Models/Producto.cs
@@ -1,13 +1,21 @@
-﻿namespace ProductosNET.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProductosNET.Models
 {
     public class Producto
     {
         public int Id { get; set; }
 
+        [MaxLength(50, ErrorMessage = "El campo debe de tener 50 caracteres como máximo")]
         public string Nombre { get; set; } = null!;
 
+        [DataType(DataType.MultilineText)]
+        [MaxLength(500, ErrorMessage = "El campo debe de tener 500 caracteres como máximo")]
         public string Descripcion { get; set; } = null!;
 
+        [Column(TypeName = "decimal(18,2)")]
+        [DisplayFormat(DataFormatString = "{0:C2}")]
         public decimal Precio { get; set; }
 
     }
