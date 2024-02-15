30e237a3d90f4d5a33794fac5b417b96f07cce11
andriusmw <andriusmw@yahoo.es>
2024-02-15 16:18:25 +0100

    Clase producto

---
+++ ProductosNET/Models/Producto.cs
@@ -0,0 +1,14 @@
+﻿namespace ProductosNET.Models
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; } = null!;
+
+        public string Descripcion { get; set; } = null!;
+
+        public decimal Precio { get; set; }
+
+    }
+}
